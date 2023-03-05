5d3703d226447e7fe1c9cd0479af70b820c961a8
thallysvic17 <thallysvic17@gmail.com>
2023-03-05 17:00:16 -0300

    End

---
+++ Js/valid.js
@@ -0,0 +1,7 @@
+        document.querySelector('form').addEventListener('submit', function(event) {
+          // Impedir que o formulário seja enviado e a página seja recarregada
+        event.preventDefault()
+          // Código para enviar o formulário aqui
+          // Adicionar a mensagem de sucesso
+        alert('teste')
+        });
