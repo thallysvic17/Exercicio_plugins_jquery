cb10e2c24a06fb719fb75d4bb713808c60262b32
thallysvic17 <thallysvic17@gmail.com>
2023-03-07 10:41:10 -0300

    Commit

--- Js/valid.js
+++
@@ -1,7 +0,0 @@
-        document.querySelector('form').addEventListener('submit', function(event) {
-          // Impedir que o formulário seja enviado e a página seja recarregada
-        event.preventDefault()
-          // Código para enviar o formulário aqui
-          // Adicionar a mensagem de sucesso
-        alert('teste')
-        });
